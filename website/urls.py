bcb9d0a15dc7d3aed78af6e5ea1ebd58e4300d1e
Krishna Chandran <mail2krishnachandran@gmail.com>
2023-05-09 14:26:25 -0400

    Base app

--- website/urls.py
+++ website/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.home, name='home' ),
     # path('login/', views.login_user, name='login' ),
     path('logout/', views.logout_user, name='logout' ),
-    path('register/', views.register, name='register' )
+    path('register/', views.register_user, name='register' ),
+    path('record/<int:pk>', views.customer_record, name='record' ),
+    path('add_record/',views.add_record, name='add_record'),
+    path('password_reset/',views.password_reset, name='password_reset')
 ]
--- website/views.py
+++ website/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect 
 from django.contrib.auth import authenticate, login, logout 
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record 
 
 # Create your views here.
 def home(request):
-    #Check to see if logging in
+    records = Record.objects.all()
+    #Check to see if logging
     if request.method == 'POST':
         user_name = request.POST['username']
         pass_word = request.POST['password']
@@ -18,7 +21,7 @@ def home(request):
             messages.success(request, "There was an error logging in")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
 
 
 def logout_user(request):
@@ -26,5 +29,33 @@ def logout_user(request):
     messages.success(request, "You have been logged out") 
     return render(request, 'home.html', {})
 
-def register(request):
-    return render(request,"register.html", {})
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username,password=password)
+            login(request, user)
+            messages.success(request, "You have successfully registered")
+            return redirect('home')
+
+    else:
+        form = SignUpForm()
+        return render(request,"register.html", {'form':form})
+    return render(request,"register.html", {'form':form})
+
+def password_reset(request):
+    return render(request,"password_reset.html")
+
+def customer_record(request,pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request,'record.html',{'customer_record':customer_record})
+    else:
+        messages.success(request,"You must be logged in to view the records")
+        return redirect('home')
+    
+def add_record(request):
+    return render(request,'add_record.html',{}) 
